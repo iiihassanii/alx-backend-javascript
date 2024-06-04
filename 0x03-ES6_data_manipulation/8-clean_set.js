caa246d31f32216fc6bdae22878d801172f481d3
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-04 13:24:22 +0300

    task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') return ('');
+  let string = '';
+  for (const element of set) {
+    if (element.startsWith(startString)) {
+      const reelement = element.replace(startString, '');
+      string += `${reelement}-`;
+    }
+  }
+  string = string.slice(0, -1);
+  return string;
+}
