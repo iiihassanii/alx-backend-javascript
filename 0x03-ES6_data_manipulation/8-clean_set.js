8a175ebdf83f4cb3a39afd18ac039987938b4be4
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-04 13:34:31 +0300

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,10 @@
 export default function cleanSet(set, startString) {
   if (startString === '') return ('');
-  let string = '';
-  for (const element of set) {
-    if (element.startsWith(startString)) {
-      string += `${element.slice(startString.length)}-`;
+  const result = [];
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      result.push(item.slice(startString.length));
     }
   }
-  if (string !== '') {
-    string = string.slice(0, -1);
-  }
-  return string;
+  return result.join('-');
 }
