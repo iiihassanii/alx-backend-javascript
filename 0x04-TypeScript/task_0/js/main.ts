fcc4eb6738077cddc8ed55fda31667d1006b2fd7
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-05 13:25:46 +0300

    task 0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student{
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "string",
+  lastName: "string",
+  age: 24,
+  location: "string"
+}
+
+const student2: Student = {
+  firstName: "string",
+  lastName: "string",
+  age: 25,
+  location: "string"
+}
+
+const studentsList: Student[] = [student1, student2];
