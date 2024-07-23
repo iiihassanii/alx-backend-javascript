42a1b4c26afadfc3a5d8741f1e87ae853433f8f6
iiihassanii <hassanabdelazim.max@gmail.com>
2024-07-23 16:17:57 +0300

    task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+console.log("Welcome to Holberton School, what is your name?\n")
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
