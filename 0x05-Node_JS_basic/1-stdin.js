697b0e96bfff4ff8933bb2592fb6212c40be2046
iiihassanii <hassanabdelazim.max@gmail.com>
2024-07-23 16:26:28 +0300

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,17 @@
 console.log("Welcome to Holberton School, what is your name?\n")
 
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
+process.stdin.setEncoding('utf8');
 
-  if (name) {
-    process.stdout.write(`Your name is: ${name}`);
-  }
-});
+process.stdin.on('data', (input) => {
+  // Trim the input to remove any extra newlines or spaces
+  const name = input.trim();
 
+  // Display the user's input
+  console.log(`Your name is: ${name}`);
 
-process.stdin.on('end', () => {
-  process.stdout.write('End');
+  // Display the closing message and end the program
+  console.log('This important software is now closing');
+
+  // End the stdin input stream
+  process.stdin.end();
 });
